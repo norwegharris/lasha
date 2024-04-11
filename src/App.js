a08c8a9a6de60419d5110f2122e2c8b53a793b82
norwegharris <kukhaleishvili.reziko@gmail.com>
2024-04-11 14:38:12 +0400

    innitial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [activeButton, setActiveButton] = useState("flight");
+
+  function handleActive(buttonName) {
+    setActiveButton(buttonName);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Purpose>
+        <>
+          <Button
+            active={activeButton === "flight"}
+            onHandleActive={() => handleActive("flight")}
+          >
+            Flight
+          </Button>
+          <Button
+            active={activeButton === "stay"}
+            onHandleActive={() => handleActive("stay")}
+          >
+            Stays
+          </Button>
+          <Button
+            active={activeButton === "car"}
+            onHandleActive={() => handleActive("car")}
+          >
+            Car Rentals
+          </Button>
+        </>
+      </Purpose>
+      <SelectOptions>
+        <SelectOption option={["One-Way", "Two-Ways"]} />
+        <SelectOption option={["1 Adult", "2 Adults", "3 Adults"]} />
+        <SelectOption option={["Economy", "Business"]} />
+      </SelectOptions>
+      <Forms />
+      <Button className={"selectBtn"}>Search</Button>
     </div>
   );
 }
 
-export default App;
+function Purpose({ children }) {
+  return <div className="purpose">{children}</div>;
+}
+
+function Button({ children, active, className = "btn", onHandleActive }) {
+  return (
+    <button
+      onClick={onHandleActive}
+      className={active ? `${className} active` : className}
+    >
+      {children}
+    </button>
+  );
+}
+
+function SelectOptions({ children }) {
+  return <div className="selectOptions">{children}</div>;
+}
+function SelectOption({ option }) {
+  return (
+    <div>
+      <select className="select">
+        {option.map((n) => (
+          <option key={n}>{n}</option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+function Forms() {
+  return (
+    <div className="forms">
+      <Form placeholder={"from:"} type={"text"} />
+      <Form placeholder={"to:"} type={"text"} />
+      <Form placeholder={"calendar"} type={"date"} />
+    </div>
+  );
+}
+
+function Form({ placeholder, type }) {
+  return <input type={type} placeholder={placeholder} className="form" />;
+}
